e30a9abb457f2b445d027b44d50660d847cc8851
lawr <lawrace.rsp@gmail.com>
2020-12-11 18:51:01 +0800

    fix for arch linux
    1. merge some fixes from https://github.com/bilguun0203/WSL-ArchLinux
    2. add passwd step when install

--- DistroLauncher/DistributionInfo.cpp
+++ DistroLauncher/DistributionInfo.cpp
@@ -9,21 +9,36 @@ bool DistributionInfo::CreateUser(std::wstring_view userName)
 {
     // Create the user account.
     DWORD exitCode;
-    std::wstring commandLine = L"/usr/sbin/adduser --quiet --gecos '' ";
+    // std::wstring commandLine = L"/usr/sbin/adduser --quiet --gecos '' ";
+    std::wstring commandLine = L"/usr/bin/useradd -m -g users -s /bin/bash ";
     commandLine += userName;
     HRESULT hr = g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
     if ((FAILED(hr)) || (exitCode != 0)) {
         return false;
     }
 
-    // Add the user account to any relevant groups.
-    commandLine = L"/usr/sbin/usermod -aG adm,cdrom,sudo,dip,plugdev ";
+    // Add user passwod
+    commandLine = L"/usr/bin/passwd ";
     commandLine += userName;
     hr = g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
     if ((FAILED(hr)) || (exitCode != 0)) {
 
         // Delete the user if the group add command failed.
-        commandLine = L"/usr/sbin/deluser ";
+        commandLine = L"/usr/bin/userdel ";
+        commandLine += userName;
+        g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
+        return false;
+    }
+
+    // Add the user account to any relevant groups.
+    // commandLine = L"/usr/sbin/usermod -aG adm,cdrom,sudo,dip,plugdev ";
+    commandLine = L"/usr/bin/usermod -aG wheel,storage,power ";
+    commandLine += userName;
+    hr = g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
+    if ((FAILED(hr)) || (exitCode != 0)) {
+
+        // Delete the user if the group add command failed.
+        commandLine = L"/usr/bin/userdel ";
         commandLine += userName;
         g_wslApi.WslLaunchInteractive(commandLine.c_str(), true, &exitCode);
         return false;
